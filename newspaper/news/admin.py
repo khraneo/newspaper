3ae8ad828b55159d3e312f7b822e9ff0c07cab92
Khorez <khorez@gmail.com>
2016-05-27 15:39:02 +0200

    include event administration

--- newspaper/news/admin.py
+++ newspaper/news/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from news.models import News
+from news.models import News, Event
 
 
 class NewsAdmin(admin.ModelAdmin):
     pass
 
+class EventAdmin(admin.ModelAdmin):
+    pass
+
 
 admin.site.register(News, NewsAdmin)
+admin.site.register(Event, EventAdmin)
